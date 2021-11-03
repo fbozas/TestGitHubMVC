4dbdb5213c65e1b0ac33f440fa4ab6d07fccee13
Dimitris Karatzas <dimkaratz85@gmail.com>
2021-11-03 18:42:16 +0200

    Add Computer Class

---
+++ TestGitHubMVC/Models/Computer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestGitHubMVC.Models
+{
+    public class Computer
+    {
+        public string CPU { get; set; }
+        public string Motherboard { get; set; }
+        public string Ram { get; set; }
+        public string GPU { get; set; }
+        public string PSU { get; set; }
+    }
+}
