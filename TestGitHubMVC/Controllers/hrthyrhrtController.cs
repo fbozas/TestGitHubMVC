71b73c0de181ddc1eee5ec40b1de391ebdd406fd
Gaster89 <jim_gasteratos@hotmail.com>
2021-11-03 18:41:57 +0200

    gaster89

---
+++ TestGitHubMVC/Controllers/hrthyrhrtController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestGitHubMVC.Controllers
+{
+    public class hrthyrhrtController : Controller
+    {
+        // GET: hrthyrhrt
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
